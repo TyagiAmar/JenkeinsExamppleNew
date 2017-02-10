bd7a09de24b94f16408e9611a30d7c1c70a66d9b
amar.tyagi <amar.tyagi@kelltonteck.com>
2017-02-10 16:20:14 +0530

    new

---
+++ app/src/androidTest/java/amar/tyagi/com/jenkinsnew/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package amar.tyagi.com.jenkinsnew;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("amar.tyagi.com.jenkinsnew", appContext.getPackageName());
+    }
+}
